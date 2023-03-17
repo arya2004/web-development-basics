98c89cdb6962bcb7ec677b14d1d14eccad1676e1
arya2004 <arya20j4@gmail.com>
2023-03-17 12:58:10 +0530

    v4.432.342.4

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -4,8 +4,13 @@ const path = require('path');
 const mongoose = require('mongoose');
 //const methodOverride = require('method-override')
 
+mongoose.connect()
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname,'views'));
+
 app.get('/', (req,res)=>{
-    res.send("new mwe")
+    res.render('home')
 })
 
 
