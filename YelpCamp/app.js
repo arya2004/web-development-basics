db321bb4b6783170c1e51336c42fa13d3ca61536
arya2004 <arya20j4@gmail.com>
2023-03-18 22:53:36 +0530

    done for 2day

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const Campground = require('./models/campground');
 //const methodOverride = require('method-override')
 
-mongoose.connect()
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", ()=>{
+    console.log("Database connectef")
+});
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,'views'));
@@ -13,6 +20,16 @@ app.get('/', (req,res)=>{
     res.render('home')
 })
 
+app.get('/makecampground', async (req,res)=>{
+    const camp = new Campground({
+        title: 'my backyard',
+        price: '231.321',
+        description: 'kek',
+        location: 'austin'
+    })
+    await camp.save();
+    res.send(camp)
+})
 
 
 
