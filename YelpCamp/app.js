a6128db15b6bb4915b6e79343aa9dbce76249afe
arya2004 <arya20j4@gmail.com>
2023-04-08 19:06:46 +0530

    today

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -7,7 +7,7 @@ const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError')
-
+const Review = require('./models/review')
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
 app.use(express.urlencoded({extended: true}))
@@ -52,7 +52,8 @@ app.post('/campgrounds',catchAsync( async (req,res, next)=>{
 
 
 app.get('/campgrounds/:id',catchAsync( async (req,res)=>{
-    const campground = await Campground.findById(req.params.id)
+    const campground = await Campground.findById(req.params.id).populate('reviews');
+   
 
     res.render('campgrounds/show', {campground})
 }))
@@ -77,6 +78,22 @@ app.delete('/campgrounds/:id',catchAsync( async (req,res)=>{
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
+}))
+
+app.post('/campgrounds/:id/reviews', catchAsync(async(req,res)=>{
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review)
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+}))
+
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async(req,res)=>{
+    const{ id, reviewId} = req.params
+    await Campground.findByIdAndUpdate(id, {$pull: {reviews: reviewId}})
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
 }))
 
 app.all('*',(req,res,next) =>{
